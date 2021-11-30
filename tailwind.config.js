42d03b40f101381d7877b8f826a0639b400d3279
unknown <richernote@gmail.com>
2021-11-29 16:57:28 -0800

    installed tailwind locally. added tailwind.config file and added custom colours updated file organization with public and src

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        eggshell: '#F5F5F6',
+        shellpink: '#E4B8AD',
+      },
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
